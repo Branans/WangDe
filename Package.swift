8044b80195d08ac4b5687ac476b666c0fdd7c192
Branans <hetukagimani@gmail.com>
2026-01-04 22:10:41 -0800

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,21 @@
+import PackageDescription
+
+let version = "1.0.8"
+let checksum = "9b3beec3e99b97af57ddee01bb121cdc7bcb94520e3f0c963da758358867b7bd"
+
+let package = Package(
+    name: "WangDe",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "ShadowSDK", targets: ["ShadowSDKXCFramework"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ShadowSDKXCFramework",
+            url: "https://github.com/balvenie-ios/ShadowSDK/releases/download/\(version)/ShadowSDK.xcframework.zip",
+            checksum: checksum
+        ),
+    ]
+)
